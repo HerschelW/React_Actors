de7a0a11537dd91a943b75c5e2cdd9ff80d6b8e2
HerschelW <herschelw88@gmail.com>
2020-06-09 12:20:05 -0500

    initial commit

---
+++ src/About/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class About extends Component {
+   render() {
+      return (
+         <div className="About">
+            <h2>This is a sample react app!</h2>
+         </div>
+      );
+   }
+}
+
+export default About;
---
+++ src/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+   render() {
+      return (
+         <div className="Contact">
+            <h2>PHONE: 1-800-WE-REACT.</h2>
+            <h2>Email: info@ReactActors.com</h2>
+         </div>
+      );
+   }
+}
+
+export default Contact;
